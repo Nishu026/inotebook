fa4db9bb00c000ba3c50d6436e23ffe879edcc73
Nishu026 <pmnishu.2000@gmail.com>
2024-03-20 20:46:26 +0530

    React Components and Routing Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Home from './components/Home';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 function App() {
   return (
+    <Router>
     <div className="App">
+      <Navbar/>
      <h1> Welcome to inotebook</h1>
+     
+            <Routes>
+                <Route exact path="/" element={<Home />}/>
+                <Route exact path="/about" element={<About/>}/>
+            </Routes>
+       
     </div>
+    </Router>
   );
 }
 
