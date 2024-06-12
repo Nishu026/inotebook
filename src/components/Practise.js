9971655050a4e0dcd6d9f8c6c93005b93d0bde87
Nishu026 <pmnishu.2000@gmail.com>
2024-06-12 18:53:13 +0530

    newly added

--- src/components/Practise.js
+++ src/components/Practise.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Practise = () => {
   return (
-    <div>Pull request practise</div>
+    <div>Pull request practise
+
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi autem expedita nobis suscipit! Veniam cumque iure accusamus pariatur at accusantium, animi vel iusto consectetur quis, beatae voluptates? Ex vitae vero vel? Fugit, aspernatur dolore.
+    </div>
   )
 }
 
